1c62a714f157f26b0ffd7f014fb6e57aab2de141
John Attai Yinusa <donattai1032@gmail.com>
2024-10-14 13:09:09 +0100

    change samesite on session to none

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
+      sameSite: "none",
     },
   })
 );
